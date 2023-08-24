b644c3c5602a207d3475a46771510d55625835d5
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-08-24 11:26:53 -0300

    animate quiz box with shake

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Alert, ScrollView, View } from 'react-native';
+import Animated, { Easing, interpolate, useAnimatedStyle, useSharedValue, withSequence, withTiming } from 'react-native-reanimated';
 
 import { useNavigation, useRoute } from '@react-navigation/native';
 
@@ -8,11 +9,11 @@ import { styles } from './styles';
 import { QUIZ } from '../../data/quiz';
 import { historyAdd } from '../../storage/quizHistoryStorage';
 
+import { ConfirmButton } from '../../components/ConfirmButton';
 import { Loading } from '../../components/Loading';
+import { OutlineButton } from '../../components/OutlineButton';
 import { Question } from '../../components/Question';
 import { QuizHeader } from '../../components/QuizHeader';
-import { ConfirmButton } from '../../components/ConfirmButton';
-import { OutlineButton } from '../../components/OutlineButton';
 
 interface Params {
   id: string;
@@ -27,10 +28,18 @@ export function Quiz() {
   const [quiz, setQuiz] = useState<QuizProps>({} as QuizProps);
   const [alternativeSelected, setAlternativeSelected] = useState<null | number>(null);
 
+  const shake = useSharedValue(0)
+
   const { navigate } = useNavigation();
 
   const route = useRoute();
   const { id } = route.params as Params;
+
+  const shakeStyleAnimated = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: interpolate(shake.value, [0, 0.5, 1, 1.5, 2, 2.5, 3], [0, -15, 0, 15, 0, -15, 0]) }]
+    }
+  })
 
   function handleSkipConfirm() {
     Alert.alert('Pular', 'Deseja realmente pular a questão?', [
@@ -69,6 +78,8 @@ export function Quiz() {
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
       setPoints(prevState => prevState + 1);
+    } else {
+      shakeAnimation()
     }
 
     setAlternativeSelected(null);
@@ -88,6 +99,10 @@ export function Quiz() {
     ]);
 
     return true;
+  }
+
+  function shakeAnimation() {
+    shake.value = withSequence(withTiming(3, { duration: 400, easing: Easing.bounce }), withTiming(0))
   }
 
   useEffect(() => {
@@ -118,12 +133,14 @@ export function Quiz() {
           totalOfQuestions={quiz.questions.length}
         />
 
-        <Question
-          key={quiz.questions[currentQuestion].title}
-          question={quiz.questions[currentQuestion]}
-          alternativeSelected={alternativeSelected}
-          setAlternativeSelected={setAlternativeSelected}
-        />
+        <Animated.View style={shakeStyleAnimated}>
+          <Question
+            key={quiz.questions[currentQuestion].title}
+            question={quiz.questions[currentQuestion]}
+            alternativeSelected={alternativeSelected}
+            setAlternativeSelected={setAlternativeSelected}
+          />
+        </Animated.View>
 
         <View style={styles.footer}>
           <OutlineButton title="Parar" onPress={handleStop} />
